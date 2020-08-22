2ed5705275bf556c6cda40f771a8f37f3a899804
david-osas <azamegbeosarumense@gmail.com>
2020-08-22 14:48:02 +0100

    Created first component in react native

---
+++ components/AddEntry.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { getMetricMetaInfo } from '../utils/helpers'
+
+function AddEntry(){
+
+  return (
+    <View>
+      <Text>Add entry</Text>
+    </View>
+  )
+}
+
+export default AddEntry
